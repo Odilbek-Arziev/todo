450fecd48a9940040866d36b770f1b8e9570aaa3
Odilbek Arziev <odarziev@mail.ru>
2024-03-03 13:16:22 +0300

    initial commit

---
+++ app/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from .models import Todo
+
+
+def home(request):
+    todos = Todo.objects.all()
+    new_todo = request.POST.get("todo")
+    todo_pk = request.GET.get("todo")
+
+    if todo_pk:
+        todo = todos.get(pk=todo_pk)
+        todo.is_done = False if todo.is_done else True
+        todo.save()
+
+        return redirect("app:home")
+
+    if request.method == "POST" and new_todo:
+        Todo.objects.create(text=new_todo, is_done=False)
+        return redirect("app:home")
+
+    stat = {
+        "all": todos.count(),
+        "finished": todos.filter(is_done=True).count(),
+        "open": todos.filter(is_done=False).count(),
+    }
+
+    context = {"todos": todos, "stat": stat}
+    return render(request, "home.html", context)
+
+
+def delete_todo(request, pk):
+    if request.method == "POST":
+        todo = Todo.objects.get(pk=pk)
+        todo.delete()
+        return redirect("app:home")
+    return render(request, "delete_todo.html", {})
+
+
+def edit_todo(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    if request.method == "POST":
+        todo.text = request.POST.get("todo")
+        todo.save()
+        return redirect("app:home")
+    return render(request, "edit_todo.html", {"todo": todo})
